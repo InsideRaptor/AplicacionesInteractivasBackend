83a7e96b692c57ba0f6840412c0c5811747598f2
InsideRaptor <88407062+InsideRaptor@users.noreply.github.com>
2023-04-27 11:49:39 -0300

    update anottations

--- src/main/java/com/example/tp/models/Libro.java
+++ src/main/java/com/example/tp/models/Libro.java
@@ -1,24 +1,36 @@
 package com.example.tp.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.Data;
 import org.jetbrains.annotations.NotNull;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(force = true)
 @Entity
+@Table(name = "Libro")
 public class Libro {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
     private int id;
     @NotNull
+    @Column(name = "Titulo")
     private String titulo;
     @NotNull
+    @Column(name = "Descripcion")
+    private String descripcion;
+    @NotNull
+    @Column(name = "Autor")
     private String autor;
     @NotNull
+    @Column(name = "Editorial")
     private String editorial;
+    @NotNull
+    @Column(name = "Estante")
+    @ManyToOne
+    @JoinColumn(name = "Libros")
+    private Estante estante;
 }
